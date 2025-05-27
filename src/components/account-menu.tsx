e8ff132b4bd977a32d3d9ce6e5f2c79820894073
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-27 11:30:00 -0300

    feat: enhance AccountMenu with loading states and add Skeleton component for better UX

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -12,16 +12,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryFn: getProfile,
     queryKey: ['profile'],
   })
-  const { data: manegedRestaurant } = useQuery({
-    queryFn: getManagedRestaurant,
-    queryKey: ['managed-restaurant'],
-  })
+  const { data: manegedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryFn: getManagedRestaurant,
+      queryKey: ['managed-restaurant'],
+    })
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -29,16 +31,29 @@ export function AccountMenu() {
           variant={'outline'}
           className="flex items-center gap-2 select-none"
         >
-          {manegedRestaurant?.name}
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            manegedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-muted-foreground text-xs font-normal">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-muted-foreground text-xs font-normal">
+                {profile?.email}
+              </span>
+            </>
+          )}
           <DropdownMenuSeparator />
           <DropdownMenuItem>
             <Building className="mr-2 h-4 w-4" />
