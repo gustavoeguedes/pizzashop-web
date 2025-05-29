7eac6a9393375c92004a6b4dccc72b8d395298a1
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-29 13:02:37 -0300

    feat: add loading skeleton for orders table to enhance user experience during data fetching

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,6 +14,7 @@ import {
 } from '../../../components/ui/table'
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrdersTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSeachParams] = useSearchParams()
@@ -27,7 +28,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -73,6 +74,7 @@ export function Orders() {
                     return <OrderTableRow key={order.orderId} order={order} />
                   })}
               </TableBody>
+              {isLoadingOrders && <OrdersTableSkeleton />}
             </Table>
           </div>
           {result && (
